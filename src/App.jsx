c9d384c97545cd6e70feca822948817bbba52902
Subodh Kudle <subodhkudle@gmail.com>
2026-01-29 20:41:34 +0530

    chore: migrate to gh-pages deployment with hash routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useMemo, Suspense } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Lenis from '@studio-freight/lenis';
 import { usePreloadImages } from './hooks/usePreloadImages';
 import LoadingScreen from './components/LoadingScreen';
